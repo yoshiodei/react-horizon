71766b4ea60cfea4567b4b7b3c020ed4b905dc21
yoshiodei <yoshiodei@yahoo.com>
2021-06-19 22:48:25 +0000

    add Footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+
+class Footer extends Component {
+    render() { 
+        return ( 
+            <div className="row">
+         <footer>
+             <div className="col-md-4">
+                 <h5 className="text-center">Lorem ipsum dolor.</h5>
+                 <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, officiis odio reiciendis dolor sit amet, consectetur adipisicing elit.</p>
+             </div>
+             <div className="col-md-4">
+                 <h5 className="text-center">Lorem ipsum dolor.</h5>
+                 <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, officiis odio reiciendis consectetur adipisicing elit.</p>
+             </div>
+             <div className="col-md-4">
+                 <h5 className="text-center">Lorem ipsum dolor.</h5>
+                 <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, officiis odio reiciendis ipsum dolor sit amet, consectetur.</p>
+             </div>        
+         </footer>
+    </div>
+         );
+    }
+}
+ 
+export default Footer;
